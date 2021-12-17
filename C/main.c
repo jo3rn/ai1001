4bd0934c0e43ff9ad73c1892bca5905971a29dbb
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-12-17 17:13:52 +0100

    Call sorting and printing array in main

--- C/main.c
+++ C/main.c
@@ -1,6 +1,26 @@
+#include "printing.h"
+#include "sorting.h"
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World!\n");
+  int arrSelection[] = {42, 420, 1337, 0, -666, 7};
+  int nSelection = sizeof(arrSelection) / sizeof(arrSelection[0]);
+
+  int arrInsertion[] = {42, 420, 1337, 0, -666, 7};
+  int nInsertion = sizeof(arrInsertion) / sizeof(arrInsertion[0]);
+
+  int arrBubble[] = {42, 420, 1337, 0, -666, 7};
+  int nBubble = sizeof(arrBubble) / sizeof(arrBubble[0]);
+
+  printf("Let's sort with selection sort!\n");
+  /* everything cool here */
+  selectionSort(arrSelection, nSelection);
+  printArray(arrSelection, nSelection);
+
+  /* to be implemented with TDD */
+  printf("Let's sort with insertion sort!\n");
+  insertionSort(arrInsertion, nInsertion);
+  printArray(arrInsertion, nInsertion);
+
   return 0;
 }
