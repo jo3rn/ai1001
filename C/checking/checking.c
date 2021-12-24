20bf52e6e627d07a73d81c6cff8246eee511906d
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2021-12-24 10:02:57 +0100

    feat: implement isPalindrome(...) function

--- C/checking/checking.c
+++ C/checking/checking.c
@@ -1,7 +1,12 @@
 #include "checking.h"
 
 int isPalindrome(char str[]) {
-  // TODO: implementation
-  return 0;
-
+  int length;
+  for (length = 0; str[length]; ++length) // Get String Length
+    continue;
+  for (int i = 0, j = length - 1; j - i > 0; ++i, --j) {
+    if (str[i] != str[j])
+      return 0;
+  }
+  return 1;
 }
