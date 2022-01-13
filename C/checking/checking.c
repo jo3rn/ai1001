3855fa924080829c35a694b19702538fcb8b8570
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2022-01-13 16:41:03 +0100

    Implement isPalindrome with tests

--- C/checking/checking.c
+++ C/checking/checking.c
@@ -1,7 +1,47 @@
 #include "checking.h"
+#include <ctype.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+void convertToLowerCase(char string[]) {
+  do {
+    *string = (char) tolower(*string);
+  } while (*++string);
+}
+
+// see: https://stackoverflow.com/a/1726321/
+void removeSpaces(char str[]) {
+  char *d = str;
+  do {
+    while (*d == ' ') {
+      ++d;
+    }
+  } while ((*str++ = *d++));
+}
 
 int isPalindrome(char str[]) {
-  // TODO: implementation
-  return 0;
 
+  if (str == NULL) {
+    return 0;
+  }
+
+  convertToLowerCase(str);
+  removeSpaces(str);
+
+  unsigned long long len = strlen(str);
+
+  for (int i = 0; i < len / 2; i++) {
+    char left = str[i];
+    char right = str[len - 1 - i];
+
+    if (left != right) {
+      printf("%s is not a palindrome.\n", str);
+      printf("%c != %c", left, right);
+      return 0;
+    }
+  }
+
+  printf("%s is a palindrome.\n", str);
+  return 1;
 }
